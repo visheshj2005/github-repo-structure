4167c26661c9c0ef43179424f8dd77f74973e60e
Vishesh Jain <visheshj2005@gmail.com>
2025-03-24 14:41:20 +0530

    timeout issue

---
+++ gunicorn.conf.py
@@ -0,0 +1,9 @@
+# Increase timeout to 120 seconds (default is usually 30)
+timeout = 120
+
+# Increase the number of workers
+workers = 4
+
+# Enable worker recycling
+max_requests = 1000
+max_requests_jitter = 50 
